958a5cdce35afe7401bd3af69fd8cc3a1eb0d2f5
Anish gehlot <anishgehlot@gmail.com>
2025-09-09 17:37:38 +0530

    feat/enable-image-extension

--- main.tf
+++ main.tf
@@ -180,7 +180,7 @@ resource "azurerm_virtual_machine_extension" "aad_login_windows" {
   virtual_machine_id         = azurerm_windows_virtual_machine.win_vm.id
   publisher                  = "Microsoft.Azure.ActiveDirectory"
   type                       = "AADLoginForWindows"
-  type_handler_version       = "1.0"
+  type_handler_version       = "2.2.0.0"
   auto_upgrade_minor_version = true
   tags                       = var.tags
 }
